8748ff131c19cd518cc6b690a60dceab2ba183af
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-01-05 20:24:50 +0100

    updated footer display

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,27 +1,32 @@
 "use client";
 import { Footer } from "flowbite-react";
 import ReactCountryFlag from 'react-country-flag';
+import './Footer.css';
 
-export default function FooterComponent(){
-    return(
+export default function FooterComponent() {
+    return (
         <>
             <footer className="fixed bottom-0 w-full bg-violet-900 sm:flex sm:items-center sm:justify-between">
-                <Footer>            
+                <Footer>
                     <div className="w-full bg-violet-900 py-4 sm:flex sm:items-center sm:justify-between">
-                        <Footer.Copyright by="| Guillermo — Tous droits réservés" year={2025} className="ml-auto text-lg text-white font-footer"/>
+                        <Footer.Copyright
+                            by="| Guillermo — Tous droits réservés"
+                            year={2025}
+                            className="ml-auto mr-1 text-lg text-white font-footer"
+                        />
                         <ReactCountryFlag
                             countryCode="CO"
                             svg
                             style={{
-                                width: "5em",
+                                width: "3.75em",
                                 height: "1.5em",
                             }}
                             title="Colombia"
-                            className="mr-auto"
+                            className="mr-auto shiny-flag"
                         />
                     </div>
                 </Footer>
             </footer>
         </>
-    )
+    );
 }
